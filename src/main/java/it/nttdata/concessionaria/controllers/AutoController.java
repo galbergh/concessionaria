b898d3208bbd77c295da52f68a8a184e1a52a4ce
Giorgio <giorgio.alberghina@gmail.com>
2021-06-11 12:46:35 +0200

    implementazione del controller per il model auto.

---
+++ src/main/java/it/nttdata/concessionaria/controllers/AutoController.java
@@ -0,0 +1,22 @@
+package it.nttdata.concessionaria.controllers;
+
+import it.nttdata.concessionaria.repositories.AutoRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AutoController {
+
+    private final AutoRepository autoRepository;
+
+    public AutoController(AutoRepository autoRepository) {
+        this.autoRepository = autoRepository;
+    }
+
+    @GetMapping("/automobili")
+    public String getAuto(Model model){
+        model.addAttribute("automobili",autoRepository.findAll());
+        return "auto";
+    }
+}
