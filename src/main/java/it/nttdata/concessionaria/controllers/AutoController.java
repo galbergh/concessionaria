d9218fa5252962d3ba977a806d2887c7f583ef00
Giorgio <giorgio.alberghina@gmail.com>
2021-06-14 10:39:29 +0200

    aggiunta la form per l'aggiunta delle automobili

--- src/main/java/it/nttdata/concessionaria/controllers/AutoController.java
+++ src/main/java/it/nttdata/concessionaria/controllers/AutoController.java
@@ -1,12 +1,14 @@
 package it.nttdata.concessionaria.controllers;
 
 import it.nttdata.concessionaria.models.Auto;
-import it.nttdata.concessionaria.models.Concessionario;
 import it.nttdata.concessionaria.repositories.AutoRepository;
+import it.nttdata.concessionaria.repositories.ConcessionarioRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,9 +18,11 @@ import java.util.stream.Collectors;
 public class AutoController {
 
     private final AutoRepository autoRepository;
+    private final ConcessionarioRepository concessionarioRepository;
 
-    public AutoController(AutoRepository autoRepository) {
+    public AutoController(AutoRepository autoRepository, ConcessionarioRepository concessionarioRepository) {
         this.autoRepository = autoRepository;
+        this.concessionarioRepository = concessionarioRepository;
     }
 
     @GetMapping("/automobili")
@@ -33,5 +37,18 @@ public class AutoController {
         List<Auto> ans = listaAuto.stream().filter(c -> c.getConcessionario().getNome().equals(nomeConcessionario)).collect(Collectors.toList());
         model.addAttribute("automobili",ans);
         return "automobili";
+    }
+
+    @PostMapping("/addAuto")
+    //@ResponseBody
+    public String addAuto(Auto a){
+        autoRepository.save(a);
+        return "redirect:/automobili";
+    }
+
+    @GetMapping("/addAuto")
+    public String getFormAuto(Model model){
+        model.addAttribute("listaConcessionarie",concessionarioRepository.findAll());
+        return "formAuto";
     }
 }
